d50f39e3b005ea743552e8c98d01e6835ef7a7d2
Marcelo <marcelob.orlando@gmail.com>
2023-02-23 18:03:40 +0100

    "reorganized folder structure"

--- PAMFluo-dynamic_python/SP_loop.py
+++
@@ -1,9 +0,0 @@
-from SP_calibration import ex
-import numpy as np
-import time
-
-N = 3
-
-for length_SP in np.linspace(100, 300, N):
-        r = ex.run(config_updates={'length_SP': int(length_SP)})
-time.sleep(100)

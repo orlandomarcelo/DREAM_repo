9b4d5b742408b141d911b14a8a87bc929ccf59cb
Marcelo <marcelob.orlando@gmail.com>
2023-03-20 10:57:15 +0100

    added ESP read

---
+++ read_ESP_simple.py
@@ -0,0 +1,36 @@
+import serial
+import csv
+
+# Set the serial port and baud rate
+ser = serial.Serial('COM3', 115200) # Replace 'COM3' with the serial port of your ESP32 board
+ser.flushInput()
+
+# Create a CSV file and write the header row
+with open('data.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Time', 'Data'])
+
+# Read serial data and save it to the CSV file
+while True:
+    try:
+        # Read a line of serial data
+        line = ser.readline().decode('utf-8').rstrip()
+        
+        # Split the line into time and data values
+        values = line.split(',')
+        if len(values) == 2:
+            time = values[0]
+            data = values[1]
+            
+            # Append the time and data values to the CSV file
+            with open('data.csv', mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([time, data])
+                
+    except KeyboardInterrupt:
+        print('Keyboard interrupt')
+        break
+        
+    except:
+        print('Error occurred')
+        continue

f16ec2912a4c003b9f709156fad79af87d5b500a
Marcelo <marcelob.orlando@gmail.com>
2023-07-24 19:00:18 +0200

    bode

---
+++ BodeClass.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import sys
+import importlib
+import glob
+from scipy.signal import find_peaks
+
+import tools
+import math_functions as mf
+
+class BodeClass:
+    def __init__(self, name, rec_string, frequency_list, median_filtering_windos_size = 1, padding = False, padding_value = None, pic_search_window = 2):
+        self.name = name
+        self.rec_string = rec_string
+        self.frequency_list = frequency_list
+        self.median_filtering_windos_size = median_filtering_windos_size
+        self.padding = padding
+        self.padding_value = padding_value
+        self.pic_search_window = pic_search_window
+        self.folder = f"C:/Users/Orlando/ownCloud - ORLANDO Marcelo@mycore.cnrs.fr/Doutorado/Dados experimentais/JTS/{name}/separated_files/"
+        self.files = self.files = sorted(glob.glob(self.folder + "*.dat"), key=os.path.getmtime)
+        self.clean_times = []
+        self.clean_data = []
+        self.recordings = []
+
+        for i, file in enumerate(self.files):
+            df = pd.read_csv(file, index_col=False)
+            aux_time = np.array(df.iloc[:,0])
+            for key in df.keys()[1:]:
+                self.recordings.append(key.replace(" ",""))   
+                self.clean_times.append(aux_time)
+                self.clean_data.append(np.array(df[key]))
+        
+        
+        self.bode_records = tools.create_record_list("6-20")
+        self.bode_times = []
+        self.bode_data = []
+
+        for i in self.bode_records:
+            index = np.where(np.array(self.recordings) == i)[0][0]
+            self.bode_times.append(self.clean_times[index])
+            self.bode_data.append(tools.median_filter(self.clean_data[index]-self.clean_data[index][0],self.median_filtering_windos_size))
+            
+        self.fund_freq = []
+        self.fund_amp = []
+        self.index_fund = []
+
+        for i, k in enumerate(self.bode_records):
+            F, A, _ = tools.FFT(self.bode_times[i]/1000, self.bode_data[i], padding, padding_value)
+            self.index_fund.append(tools.closest_index(F, frequency_list[i]))
+            peak = np.argmax(A[self.index_fund[i] - pic_search_window:self.index_fund[i] + pic_search_window]) + self.index_fund[i] - pic_search_window
+            self.fund_freq.append(F[peak])
+            self.fund_amp.append(A[peak])
+            
+            
+        
+
+
+                    
+        
---
+++ VitessePhotochimiquePWMClass.py
@@ -0,0 +1,54 @@
+from ExperimentClass import Experiment
+import tools
+import numpy as np
+
+class PhotochemicalRate(Experiment):
+    def __init__(self, name, Ek_records_str = "16-21", PWM_list = [0.01, 0.04, 0.1, 0.25, 0.5, 0.8], Flash_record_str= "7", 
+                 start = -5, stop = 25, num = 50, index_stop_fit = 26):
+        super().__init__(name, equipment = "JTS", local="IBPC", DataType=".dat", is_sub_experiment=False, parent_experiment_name=None)
+        self.Ek_records = tools.create_record_list(Ek_records_str)
+        self.PWM_list = PWM_list
+        self.Flash_records = tools.create_record_list(Flash_record_str)
+        self.start = start
+        self.stop = stop 
+        self.num = num
+        self.index_stop_fit = index_stop_fit
+        
+        self.Flash_Time = self.clean_times[self.records.index(self.Flash_records[0])]
+        self.Flash_Data = self.clean_data[self.records.index(self.Flash_records[0])]
+        
+        self.calib = self.Flash_Data[20]-self.Flash_Data[19]
+        
+        self.xfit_lin = []
+        self.yfit_lin = []
+        self.param = []
+        self.vitesse = []
+
+
+        self.Ek_time = self.clean_times[self.records.index(self.Ek_records[0])][21:self.index_stop_fit]
+
+
+        for i, k in enumerate(self.Ek_records):
+            ydata = self.clean_data[self.records.index(k)][21:index_stop_fit]
+            popt, x, y =  tools.lin_fit(self.Ek_time, ydata, start, stop, num)
+            self.xfit_lin.append(x)
+            self.yfit_lin.append(y)
+            self.param.append(popt)
+            self.vitesse.append(-1000*self.param[i][0]/self.calib)
+            
+            
+        self.intensity_rel = np.insert(self.PWM_list, 0, 0)
+        self.intensity = 1326 * np.asarray(self.PWM_list) + 11.9
+        self.intensity = np.insert(self.intensity, 0, 0)
+        self.vitesse.insert(0,0)
+        
+        popt, self.xfit_Ek_rel, self.yfit_Ek_rel = tools.Ek_fit(self.intensity_rel, self.vitesse, 0, 1, 50, p0 = [200, 0.2])
+        self.Ek_rel = popt[1]
+     
+        popt, self.xfit_Ek, self.yfit_Ek = tools.Ek_fit(self.intensity, self.vitesse, 0, 1200, 50, p0 = [200, 100])
+        self.Ek = popt[1]
+
+                
+                
+
+            
